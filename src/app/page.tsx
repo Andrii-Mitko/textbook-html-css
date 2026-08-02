e1a22c6397c6a3d45f014649869e1c71d45e28d2
Andrii-Mitko <andreymit123@gmail.com>
2026-08-02 22:52:09 +0300

    add img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,34 @@
+import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <section className={styles.container}>
-      <h1>Онлайн навчальник веброзробки</h1>
+      <div className={styles.hero}>
+        <div className={styles.content}>
+          <h1>Онлайн навчальник веброзробки</h1>
 
-      <p className={styles.description}>
-        Практичні уроки Git, HTML, CSS та сучасної розробки.
-      </p>
+          <p className={styles.description}>
+            Практичні уроки Git, HTML, CSS та сучасної розробки.
+          </p>
 
-      <Link href="/tutorials/git" className={styles.card}>
-        Почати Git
-      </Link>
+          <Link href="/tutorials/git" className={styles.card}>
+            Почати Git
+          </Link>
+        </div>
+
+        <div className={styles.imageWrapper}>
+          <Image
+            src="/cover.webp"
+            alt="Онлайн навчальник веброзробки"
+            width={1376}
+            height={768}
+            priority
+            className={styles.image}
+          />
+        </div>
+      </div>
     </section>
   );
 }
